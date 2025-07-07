cfb59f0661a9151d641388aabe3077071b64fec6
robbiem27 <eagle2b1@hotmail.com>
2025-07-07 10:24:10 -0400

    Add files via upload

---
+++ Download main.js
@@ -0,0 +1,10 @@
+
+document.getElementById('status').innerText = "100% Ready";
+
+function startRetro() {
+    alert("Retroactive Recording has been started!");
+}
+
+function startTime() {
+    alert("Time Travel has been started!");
+}
